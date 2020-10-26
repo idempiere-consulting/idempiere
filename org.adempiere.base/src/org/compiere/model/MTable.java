fecd6974bfc8852ae749153c2d8d4b8df0bc3951
hengsin <hengsin@gmail.com>
2020-10-27 01:53:10 +0800

    IDEMPIERE-4498 Performance: add cache for osgi services factory (#321)
    * IDEMPIERE-4498 Performance: add cache for osgi services factory
    
    add cache for tableName -> IModelFactory
    
    * IDEMPIERE-4498 Performance: add cache for osgi services factory
    
    implement cache for the discovery of osgi services
    
    * IDEMPIERE-4498 Performance: add cache for osgi services factory
    
    added unit test

--- org.adempiere.base/src/org/compiere/model/MTable.java
+++ org.adempiere.base/src/org/compiere/model/MTable.java
@@ -36,6 +36,7 @@ import org.adempiere.base.Service;
 import org.adempiere.model.GenericPO;
 import org.compiere.db.AdempiereDatabase;
 import org.compiere.db.Database;
+import org.compiere.util.CCache;
 import org.compiere.util.CLogger;
 import org.compiere.util.DB;
 import org.compiere.util.Env;
